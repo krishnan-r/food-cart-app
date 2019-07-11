42ca763cbefa5f725749d78652200935cd9b017c
Sushma07 <as45@iitbbs.ac.in>
2019-07-11 22:43:27 +0530

    Add existing file

--- FoodAppServer/src/main/java/appserver/http/HttpVerticle.java
+++ FoodAppServer/src/main/java/appserver/http/HttpVerticle.java
@@ -12,13 +12,13 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class HttpVerticle extends AbstractVerticle {
 
     private static final Logger logger = Logger.getLogger(HttpVerticle.class.getName());
-
 
     private void failureHandler(RoutingContext handler) {
         HttpServerResponse response = handler.response();
@@ -31,6 +31,41 @@ public class HttpVerticle extends AbstractVerticle {
             result.put("message", handler.failure().toString());
         response.end(Json.encode(result));
     }
+    protected static Connection initializeDatabase() throws SQLException, ClassNotFoundException    {
+        String dbDriver = "com.mysql.cj.jdbc.Driver";
+        String dbURL = "jdbc:mysql:// localhost:3306/";
+        String dbName = "foodcart";
+        String dbUsername = "root";
+        String dbPassword = "";
+
+        Class.forName(dbDriver);
+        Connection con = DriverManager.getConnection(dbURL + dbName,dbUsername,dbPassword);
+        return con;
+    }
+    private boolean verifyUser(String user, String password){
+        try {
+            Connection conn = initializeDatabase();
+            System.out.println("Connection Established");
+            PreparedStatement stmt = conn.prepareStatement("select password from customer_details where employee_id= ?");
+            stmt.setString(1,user);
+            // stmt.execute("insert into customer_details (name, employee_id,password, mobile,email,verified) values ('sushma','E1','qwer','9553488444','as45@iitbbs.ac.in',0)");
+            ResultSet res = stmt.executeQuery();
+            while(res.next()){
+                String pass = res.getString("password");
+                if(password.equals(pass)) {
+                    conn.close();
+                    return true;
+                }
+
+            }
+            conn.close();
+
+        }
+        catch (Exception e){
+            System.out.print("Error connecting to Database "+e.getMessage());
+        }
+        return false;
+    }
 
     private void loginHandler(RoutingContext context) {
         logger.log(Level.INFO, "loginHandler request received");
@@ -38,14 +73,20 @@ public class HttpVerticle extends AbstractVerticle {
         if (!params.containsKey("username") || !params.containsKey("password")) {
             context.fail(1);
         } else {
-            //TODO Perform authentication here.
-            HttpServerResponse response = context.response();
-            response.putHeader("content-type", "application/json; charset=utf-8");
-            JsonObject js = new JsonObject();
-            js.put("logged_in", true);
-            js.put("token", "ABCD");
-            String json = Json.encode(js);
-            response.end(json);
+            // Authentication
+            boolean authenticate = verifyUser(params.getString("username"),params.getString("password"));
+           if(authenticate) {
+               HttpServerResponse response = context.response();
+               response.putHeader("content-type", "application/json; charset=utf-8");
+               JsonObject js = new JsonObject();
+               js.put("logged_in", true);
+               js.put("token", "ABCD");
+               String json = Json.encode(js);
+               response.end(json);
+           }
+           else{
+               logger.log(Level.INFO, "Authentication Failed");
+           }
         }
     }
 
@@ -90,7 +131,7 @@ public class HttpVerticle extends AbstractVerticle {
 
 
         server.requestHandler(router);
-        server.listen(config().getInteger("port", 80), ar -> {
+        server.listen(config().getInteger("port", 83), ar -> {
             if (ar.succeeded()) {
                 logger.log(Level.INFO, "HttpVerticle initialization completed.");
                 future.complete();
