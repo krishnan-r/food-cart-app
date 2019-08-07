fe1283e2becf7ea36029c5df2598b2251e95282e
Sushma07 <as45@iitbbs.ac.in>
2019-08-07 08:29:00 +0530

    Cleaned the code and added navigation control

---
+++ FoodApp/app/src/main/java/com/example/foodapp/Vendor_add_menu.java
@@ -0,0 +1,14 @@
+package com.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Vendor_add_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_add_menu);
+    }
+}
---
+++ FoodApp/app/src/main/java/com/example/foodapp/Vendor_orders.java
@@ -0,0 +1,7 @@
+package com.example.foodapp;
+
+public class Vendor_orders {
+
+    public String item_name;
+    public int no_of_orders;
+}
